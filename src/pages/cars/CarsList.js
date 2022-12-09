5f51137cd30765cd17b184620f3b7e0f88beb379
Erik <erikhgm@gmail.com>
2022-12-09 15:32:51 +0000

    feat: add states variables and hook function to carslist

--- src/pages/cars/CarsList.js
+++ src/pages/cars/CarsList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
+function CarsList({ message, filter = "" }) {
+  const [cars, setCars] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
 
-function CarsList() {
+  useEffect(() => {
+    const fetchCars = async () => {
+      try {
+        const { data } = await axiosReq.get(`/cars/?${filter}`);
+        setCars(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchCars();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
