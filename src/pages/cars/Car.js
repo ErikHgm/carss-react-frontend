f916057aeeaaac88b1beb2d137ce88d691715d88
Erik <erikhgm@gmail.com>
2022-12-09 11:11:34 +0000

    feat: add card body to car component

--- src/pages/cars/Car.js
+++ src/pages/cars/Car.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Car = (props) => {
@@ -26,7 +29,22 @@ const Car = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
-  return <div>Car ad placeholder</div>;
+  return (
+    <Card>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && carPage && "..."}
+          </div>
+        </Media>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Car;
