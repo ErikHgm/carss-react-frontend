b72cb415399bc19aa86204413dc801f989f1b2a0
Erik <erikhgm@gmail.com>
2022-12-09 08:28:30 +0000

    feat: add useeffect hook and handlemount function

--- src/pages/cars/CarPage.js
+++ src/pages/cars/CarPage.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function CarPage() {
+  const { id } = useParams();
+  const [car, carPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: car }] = await Promise.all([
+          axiosReq.get(`/cars/${id}`),
+        ]);
+        carPost({ results: [car] });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
