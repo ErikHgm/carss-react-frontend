7a02b0a7ffe1ad4d19ad5686e24b276c5aad8380
Erik <erikhgm@gmail.com>
2022-12-09 08:22:00 +0000

    feat: add carpage basic structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import CarCreateForm from "./pages/cars/CarCreateForm"
+import CarCreateForm from "./pages/cars/CarCreateForm";
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import CarPage from "./pages/cars/CarPage";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-      <Switch>
+        <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/cars/create" render={() => <CarCreateForm /> } />
+          <Route exact path="/cars/create" render={() => <CarCreateForm />} />
+          <Route exact path="/cars/:id" render={() => <CarPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
