a15e1b5a4fb8f8b11cbd55245fc04646960f84a5
Erik <erikhgm@gmail.com>
2022-12-11 16:48:15 +0000

    feat: add popularprofiles return logic

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import appStyles from "../../App.module.css";
+import { Container } from "react-bootstrap";
 
 const PopularProfiles = () => {
   const [profileData, setProfileData] = useState({
@@ -28,7 +30,14 @@ const PopularProfiles = () => {
     handleMount();
   }, [currentUser]);
 
-  return <div>Top Sellers:</div>;
+  return (
+    <Container className={appStyles.Content}>
+      <p>Top Sellers:</p>
+      {popularProfiles.results.map((profile) => (
+        <p key={profile.id}>{profile.owner}</p>
+      ))}
+    </Container>
+  );
 };
 
 export default PopularProfiles;
