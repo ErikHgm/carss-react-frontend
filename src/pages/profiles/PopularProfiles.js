0827d4e504db0809a39c47b45895f7f07eb943fa
Erik <erikhgm@gmail.com>
2022-12-11 16:40:01 +0000

    feat: add popularprofiles state variables and handlemount function

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = () => {
-  return (
-    <div>Top Sellers:</div>
-  )
-}
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
-export default PopularProfiles
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return <div>Top Sellers:</div>;
+};
+
+export default PopularProfiles;
