1b879e40b8913a5f93471c2bd898e076e364015f
Erik <erikhgm@gmail.com>
2022-12-13 17:15:06 +0000

    style: add profile styles

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+const Profile = () => {};
 
-export default Profile
+export default Profile;
