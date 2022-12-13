51caf6983d6479a20fa023f6558ebe09bc142f41
Erik <erikhgm@gmail.com>
2022-12-13 17:18:17 +0000

    feat: add profile vars

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,6 +2,16 @@ import React from "react";
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
-const Profile = () => {};
+const Profile = () => {
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+
+    );
+};
 
 export default Profile;
